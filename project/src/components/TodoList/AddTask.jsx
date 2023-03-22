e46d4293929b1fc4ca5aceaca4fe4931eae15378
Rozelier Julien <rozelierjjs@gmail.com>
2023-03-22 15:46:17 +0100

    ajouter une nouvelle tâche

--- project/src/components/TodoList/AddTask.jsx
+++ project/src/components/TodoList/AddTask.jsx
@@ -26,10 +26,10 @@ const AddTask = ({ setOpenAddTask }) => {
                 <div className="absolute text-2xl right-2 top-2">
                     <ion-icon onClick={() => setOpenAddTask(false)} name="close-outline"></ion-icon>
                 </div>
-                <h2 className="font-semibold text-3xl mb-5">Ajouter une tache</h2>
+                <h2 className="font-semibold text-3xl mb-5">Ajouter une tâche</h2>
                 <small className={`${error.length > 0 && "opacity-100"} duration-300 opacity-0 text-xs text-red-600 mb-5`}>{error}</small>
                 <form onSubmit={handleSubmit} action="">
-                    <input id="title" type="text" placeholder="Votre tache ...." className="input text-sm input-bordered w-full mb-5" />
+                    <input id="title" type="text" placeholder="Votre tâche ...." className="input text-sm input-bordered w-full mb-5" />
                     <textarea id="description" className="textarea textarea-bordered w-full" placeholder="Votre description ..."></textarea>
                     <button type='submit' className="bg-accent rounded-lg px-3 py-2 text-white w-full mt-5">Ajouter</button>
                 </form>
