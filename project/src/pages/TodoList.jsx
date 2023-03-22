c7667b6dc1ab47d861a14ba6b14f2c7d42fc8932
Rozelier Julien <rozelierjjs@gmail.com>
2023-03-22 10:44:30 +0100

    suppression des fichiers

--- project/src/pages/TodoList.jsx
+++ project/src/pages/TodoList.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
-import { useSelector } from 'react-redux'
-import Tasks from '../components/TodoList/Tasks'
 
 const TodoList = () => {
     const tasks = useSelector(state => state.todo)
+    const [addTask, setAddTask] = useState(false)
     return (
         <div>
-            <Title text={"Liste des tâches"} />
-            <section className="mt-20"></section>
-            {tasks.map((task) => (
-                <Tasks key={task.id} task={task} />
-            ))}
 
         </div>
     )
