a16c0a088d32892cf3d6bb89df0b16810cc36842
Rozelier Julien <rozelierjjs@gmail.com>
2023-03-23 09:22:49 +0100

    restore

---
+++ project/src/redux/TodoListSlice.jsx
@@ -0,0 +1,30 @@
+import { createSlice, createAsyncThunk, configureStore } from "@reduxjs/toolkit"
+import Data from '../data/Todo.json'
+
+const TodoSlice = createSlice({
+    name: "todo",
+    initialState: Data,
+    reducers: {
+        addTask: (state, action) => {
+            const newTask = {
+                id: Math.random(),
+                state: false,
+                title: action.payload,
+                description: action.payload
+            }
+            state.push(newTask)
+        },
+        toggleTask: (state, action) => {
+            const task = state.find(t => t.id === action.payload);
+            task.state = !task.state
+        },
+
+    }
+})
+export const { addTask, toggleTask } = TodoSlice.actions
+
+export const store = configureStore({
+    reducer: {
+        todo: TodoSlice.reducer
+    }
+})
